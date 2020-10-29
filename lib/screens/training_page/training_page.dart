c410497047cb67712c9d7657413f113ad55adf0a
Augusto Zanetti <augusto.zanetti@profusion.mobi>
2020-10-29 00:01:08 -0300

    feat: add muscle group service

--- lib/screens/training_page/training_page.dart
+++ lib/screens/training_page/training_page.dart
@@ -1,45 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gym_app/models/muscleGroup.dart';
 import 'package:gym_app/screens/execution/execution.dart';
-
-class Data {
-  Map fetched_data = {
-    "data": [
-      {"id": "peito", "name": "Supino Reto", "level": "Iniciante"},
-      {"id": "peito", "name": "Supino Inclinado", "level": "Intermediário"},
-      {"id": "peito", "name": "Supino Declinado", "level": "Intermediário"},
-      {"id": "abdomen", "name": "Abdomen", "level": "Iniciante"},
-      {"id": "ombro", "name": "Elevação Frontal", "level": "Iniciante"},
-      {"id": "ombro", "name": "Elevação Lateral", "level": "Iniciante"},
-      {"id": "costas", "name": "Remada", "level": "Iniciante"},
-      {"id": "costas", "name": "Puxada Aberta", "level": "Iniciante"},
-      {"id": "biceps", "name": "Biceps", "level": "Iniciante"},
-      {"id": "pernas", "name": "Pernas", "level": "Iniciante"}
-    ]
-  };
-  List _data;
-
-  Data(String id) {
-    _data = fetched_data["data"]
-        .where((item) => item["id"] == id.toLowerCase())
-        .toList();
-  }
-
-  int getId(int index) {
-    return _data[index]["id"];
-  }
-
-  String getName(int index) {
-    return _data[index]["name"];
-  }
-
-  String getLevel(int index) {
-    return _data[index]["level"];
-  }
-
-  int getLength() {
-    return _data.length;
-  }
-}
+import 'package:gym_app/services/api.dart';
 
 class TrainingArguments {
   final String title;
@@ -57,41 +19,77 @@ class _TrainingPageState extends State<TrainingPage> {
   @override
   Widget build(BuildContext context) {
     final TrainingArguments args = ModalRoute.of(context).settings.arguments;
-    Data _data = new Data(args.title);
+    Future<List<ResultMuscleGroup>> _muscleGroup =
+        ApiService.fetchMuscleGroup(args.title);
 
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         title: Text(args.title),
       ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(5.5),
-        itemCount: _data.getLength(),
-        itemBuilder: (context, index) {
-          return Card(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                ListTile(
-                  leading: Icon(Icons.access_alarm),
-                  title: Text(
-                    _data.getName(index),
-                  ),
-                  subtitle: Text(_data.getLevel(index)),
+      body: FutureBuilder<List<ResultMuscleGroup>>(
+        future: _muscleGroup,
+        builder: (BuildContext context,
+            AsyncSnapshot<List<ResultMuscleGroup>> snapshot) {
+          List<Widget> children;
+          if (snapshot.hasError) {
+            children = <Widget>[
+              Icon(
+                Icons.error_outline,
+                color: Colors.red,
+                size: 60,
+              )
+            ];
+          } else {
+            children = <Widget>[
+              Center(
+                child: SizedBox(
+                  child: CircularProgressIndicator(),
+                  width: 60,
+                  height: 60,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    FlatButton(
-                      child: const Text('Iniciar'),
-                      onPressed: () {
-                        Navigator.pushNamed(context, ExecutionPage.routeName);
-                      },
+              )
+            ];
+          }
+          return Center(
+            child: snapshot.hasData
+                ? ListView.builder(
+                    padding: const EdgeInsets.all(5.5),
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            ListTile(
+                              leading: Icon(Icons.access_alarm),
+                              title: Text(snapshot.data[index].name),
+                              subtitle: Text(snapshot.data[index].level),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                FlatButton(
+                                  child: const Text('Iniciar'),
+                                  onPressed: () {
+                                    Navigator.pushNamed(
+                                        context, ExecutionPage.routeName);
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  )
+                : Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: children,
                     ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
           );
         },
       ),
--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,16 +1,33 @@
 import 'package:gym_app/models/muscle.dart';
+import 'package:gym_app/models/muscleGroup.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+final api = "http://9e3b72426154.ngrok.io/";
+
 class ApiService {
   static Future<List<ResultMuscle>> fetchMuscle() async {
-    final api = "http://9e3b72426154.ngrok.io/";
-    final response = await http.get("${api}muscle/");
-    print(response);
+    final response = await http.get("${api}muscle");
+
     if (response.statusCode == 200) {
       List<ResultMuscle> muscles;
       muscles = (json.decode(response.body) as List)
           .map((i) => ResultMuscle.fromMap(i))
+          .toList();
+
+      return muscles;
+    } else {
+      throw Exception('Requisição inválida!');
+    }
+  }
+
+  static Future<List<ResultMuscleGroup>> fetchMuscleGroup(String id) async {
+    final response = await http.get("${api}muscle-group/${id}");
+
+    if (response.statusCode == 200) {
+      List<ResultMuscleGroup> muscles;
+      muscles = (json.decode(response.body) as List)
+          .map((i) => ResultMuscleGroup.fromMap(i))
           .toList();
 
       return muscles;
